e1d812906bf7ff8e0f8e9d907f9dcf78a5ecdc00
Serafim202 <vserafim821@gmail.com>
2025-09-18 19:43:20 -0300

    Add files via upload

---
+++ projeto/android-app/app/src/main/java/com/exemplo/meuapp/RelatorioActivity.java
@@ -0,0 +1,12 @@
+package com.exemplo.meuapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RelatorioActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relatorio);
+    }
+}
